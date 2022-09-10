3896a8024e9ef34fa31dde7ae048673fbff25b9a
Bora Kim <a52447879@gmail.com>
2022-09-11 02:01:47 +0900

    [feat] 국가별 클릭 했을때 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Global, css } from "@emotion/react";
 import GlobalInfo from "./componsnes/GlobalInfo";
 import CountryList from "./componsnes/CountryList";
-import { RespinseData } from "./types/type";
+import { RespinseData, Country } from "./types/type";
 
 const App: React.FC = () => {
   const [data, setData] = useState<RespinseData | undefined>(undefined);
+  const [activeCountry, setActiveCountry] = useState<Country[]>([]);
 
   const fetchData = async () => {
     const result = await fetch("https://api.covid19api.com/summary");
@@ -18,6 +19,11 @@ const App: React.FC = () => {
     fetchData();
   }, []);
 
+  const onCountryClick = (country: Country) => {
+    setActiveCountry([...activeCountry, country]);
+    console.log(activeCountry);
+  };
+
   return (
     <div>
       <Global
@@ -29,6 +35,10 @@ const App: React.FC = () => {
         `}
       />
 
+      {activeCountry.map((aCountry) => (
+        <span>{aCountry.Country}</span>
+      ))}
+
       {data ? (
         <>
           <GlobalInfo
@@ -36,7 +46,10 @@ const App: React.FC = () => {
             newDeaths={data?.Global.NewDeaths}
             newRecovered={data?.Global.NewRecovered}
           />
-          <CountryList countries={data?.Countries} />
+          <CountryList
+            countries={data?.Countries}
+            onCountryClick={onCountryClick}
+          />
         </>
       ) : (
         "Loading..."
