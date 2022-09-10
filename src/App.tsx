bff5ab375bfde1d6fc540ccd06a0a7be7f2742de
Bora Kim <a52447879@gmail.com>
2022-09-11 00:07:05 +0900

    [feat] 타입 설정 및 나라별 코로나19 데이터 받아오기

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Country {
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  ID: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  Premium: unknown;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
 }
+
+interface GlobalData {
+  Date: string;
+  NewConfirmed: number;
+  NewDeaths: number;
+  NewRecovered: number;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  TotalRecovered: number;
+}
+
+interface RespinseData {
+  Countries: Country[];
+  Date: string;
+  Global: GlobalData;
+  ID: string;
+  Message: string;
+}
+
+const App: React.FunctionComponent = () => {
+  const [data, setData] = useState<RespinseData | undefined>(undefined);
+
+  const fetchData = async () => {
+    const result = await fetch("https://api.covid19api.com/summary");
+    const data: RespinseData = await result.json();
+    setData(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <div>App</div>;
+};
 
 export default App;
